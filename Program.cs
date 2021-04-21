48719e9260eaedf38dd8e430daccd7f5b032568f
Nick Boehret <blindestman@gmail.com>
2021-04-21 12:05:06 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
